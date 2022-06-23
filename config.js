6e23955dd231477e3511e193ff73b2ab7c1c29be
Ilyas162 <101692711+Ilyas162@users.noreply.github.com>
2022-06-23 20:48:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,12 +3,12 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 
-global.owner = ['6281220670449','0']
+global.owner = ['6285235662009','0']
 global.packname = 'Bot Whatsapp'
-global.author = 'TRASH'
+global.author = 'YASS-MD'
 global.sessionName = 'data'
 global.prefa = ['','!','.','#']
-global.ownername = 'KurrXd'
+global.ownername = 'YASS
 global.sp = '>'
 global.mess = {
     admin: 'Fitur Khusus Admin Group!',
